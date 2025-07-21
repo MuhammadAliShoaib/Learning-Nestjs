83963362d1fa871e92b2a2647546a9218d23b0c4
Muhammad Ali <malishoaib.2001@gmail.com>
2025-07-21 23:02:40 +0500

    Refactor: Update messages controller for improved structure and readability

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-    @Get('/get')
-    getMessages(){
+  messagesService: MessagesService;
 
-    }
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
 
-    @Post('/create')
-    createMessage(@Body() body :CreateMessageDto){
-        console.log(body)
-    }
+  @Get('/get')
+  getMessages() {
+    return this.messagesService.findAll();
+  }
 
-    @Get('/get/:id')
-    getMessage(@Param('id') id:string){
-        console.log(id)
+  @Post('/create')
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/get/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message Not Found');
     }
+    return message;
+  }
 }
